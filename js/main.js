1b0e8a07a24c851278ac8bc0a7444e1ba6911678
Dany serfaty <davidserfaty2315@gmail.com>
2022-02-08 16:25:54 +0200

    j'ai reussi a CREER LA PAGE LOGIN ;->* mntn il fo ke je mette ds le local storage les donner du login

--- js/main.js
+++ js/main.js
@@ -1,21 +1,31 @@
-var start = document.getElementById("start")
 var body = document.querySelector("body")
+var header = document.querySelector("header")
 var milisec = document.getElementById("milisec")
 var sec = document.getElementById("sec")
+var inscrire = document.getElementById("inscrire")
+var start = document.getElementById("start")
+var divFlou = document.getElementById("div-flou")
 var barre = document.getElementById("barre")
-var divFlou = document.getElementById("div-Flou")
-var header = document.querySelector("header")
 
-    start.addEventListener("click", function () {
-    body.style.opacity ="0.5";
-    var divLog = document.createElement("div")
-    divLog.style.width="250px"
-    divLog.style.height="250px"
-    divLog.style.backgroundColor="red"
-    divLog.style.opacity="0.5"
-    divLog.style.margin ="0px AUTO"
-    divLog.style.position="absolute"
-    body.appendChild(divLog)
 
-    
+inscrire.addEventListener("click", function () {
+divFlou.style.display="initial"
 })
+
+
+var inputNom = document.getElementById("nom")
+var inputPrenom = document.getElementById("prenom")
+var submit =document.getElementById("submit")
+submit.addEventListener("click",function () {
+    if (inputNom.value =="") {
+        alert("Remplisser votre nom pour jouer")
+    }
+
+    if ( inputPrenom.value =="") {
+        alert("Remplisser votre prenom pour jouer")
+    }
+
+    divFlou.style.display="none"
+})
+
+
