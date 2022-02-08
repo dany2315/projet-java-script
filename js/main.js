ea9743c1afa2e12ebf81e3543dbbfbef4b6df1ca
Dany serfaty <davidserfaty2315@gmail.com>
2022-02-09 00:57:06 +0200

    je m'occupe du local storage

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ var inscrire = document.getElementById("inscrire")
 var start = document.getElementById("start")
 var divFlou = document.getElementById("div-flou")
 var barre = document.getElementById("barre")
+var meilleurScore = 0
+var meilleurNiveau = 0
 
 
 inscrire.addEventListener("click", function () {
@@ -16,7 +18,34 @@ divFlou.style.display="initial"
 var inputNom = document.getElementById("nom")
 var inputPrenom = document.getElementById("prenom")
 var submit =document.getElementById("submit")
+
+class users{
+    constructor(_nom,_prenom,_niveau,_score){
+        this.nom =_nom;
+        this.prenom =_prenom;
+        this.niveau =_niveau;
+        this.score =_score;
+    }
+}
+var user=null
+var userJason = localStorage.user
+
+function verificationUser() {
+    userJason? welcome():login();
+
+    function welcome() {
+        inscrire.style.display="none"
+    }
+
+    function login() {
+        inscrire.style.display="initial"
+        
+    }
+}
+
+
 submit.addEventListener("click",function () {
+    
     if (inputNom.value =="") {
         alert("Remplisser votre nom pour jouer")
     }
@@ -24,8 +53,17 @@ submit.addEventListener("click",function () {
     if ( inputPrenom.value =="") {
         alert("Remplisser votre prenom pour jouer")
     }
+    if (inputPrenom.value!="" && inputNom.value!="") {
+        divFlou.style.display="none"
+    }
+    
 
-    divFlou.style.display="none"
+    user = new users (inputNom.value,inputPrenom.value)
+    var myjson =JSON.stringify(user)
+    localStorage.setItem("user",myjson)
+
+    verificationUser
+
 })
 
-
+//start.addEventListener("click",function)
