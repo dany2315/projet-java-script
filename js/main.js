fd7903418516de37737fe53bd8e0c92532326eb3
Dany serfaty <davidserfaty2315@gmail.com>
2022-02-09 16:03:03 +0200

    G MI LE CHRONO A L'ENVERS

--- js/main.js
+++ js/main.js
@@ -1,23 +1,29 @@
 var body = document.querySelector("body")
 var header = document.querySelector("header")
-var milisec = document.getElementById("milisec")
-var sec = document.getElementById("sec")
-var inscrire = document.getElementById("inscrire")
 var start = document.getElementById("start")
 var divFlou = document.getElementById("div-flou")
 var barre = document.getElementById("barre")
 var meilleurScore = 0
 var meilleurNiveau = 0
 
+function verificationUser() {
+    var userJason = localStorage.user
+    userJason? welcome():login();
 
-inscrire.addEventListener("click", function () {
-divFlou.style.display="initial"
-})
+    function welcome() {
+        divFlou.style.display="none" 
+    }
+
+    function login() {
+        divFlou.style.display="initial";
+    }
+
+}
+verificationUser()
 
 
 var inputNom = document.getElementById("nom")
 var inputPrenom = document.getElementById("prenom")
-var submit =document.getElementById("submit")
 
 class users{
     constructor(_nom,_prenom,_niveau,_score){
@@ -27,27 +33,15 @@ class users{
         this.score =_score;
     }
 }
+
+var submit =document.getElementById("submit")
 var user=null
-var userJason = localStorage.user
-
-function verificationUser() {
-    userJason? welcome():login();
-
-    function welcome() {
-        inscrire.style.display="none"
-    }
-
-    function login() {
-        inscrire.style.display="initial"
-        
-    }
-}
-
-
 submit.addEventListener("click",function () {
     
+
     if (inputNom.value =="") {
         alert("Remplisser votre nom pour jouer")
+
     }
 
     if ( inputPrenom.value =="") {
@@ -55,15 +49,44 @@ submit.addEventListener("click",function () {
     }
     if (inputPrenom.value!="" && inputNom.value!="") {
         divFlou.style.display="none"
+        user = new users (inputNom.value,inputPrenom.value)
+        var myjson =JSON.stringify(user)
+        localStorage.setItem("user",myjson)
     }
     
-
-    user = new users (inputNom.value,inputPrenom.value)
-    var myjson =JSON.stringify(user)
-    localStorage.setItem("user",myjson)
-
-    verificationUser
-
 })
 
-//start.addEventListener("click",function)
+
+start.addEventListener("click",interval,)
+var set = null
+const chrono={
+    sec:{val:59 , dom:document.getElementById("divsec")},
+    milisec:{val:999 , dom:document.getElementById("divmilisec")},
+}
+
+function interval() {
+    
+    set = setInterval(function () {
+        chrono.milisec.val--
+        chrono.milisec.dom.innerText=chrono.milisec.val
+        if (chrono.milisec.val<100) {
+            chrono.milisec.dom.innerText="0"+chrono.milisec.val
+        }
+        if (chrono.milisec.val<10) {
+            chrono.milisec.dom.innerText="00"+chrono.milisec.val
+        }
+        if (chrono.milisec.val==0) {
+            chrono.milisec.val=999
+            chrono.sec.val--
+            chrono.sec.dom.innerText=chrono.sec.val
+            if (chrono.sec.val<10) {
+                chrono.sec.dom.innerText="0"+chrono.sec.val
+            }
+        }
+        if (chrono.milisec.val==0 && chrono.sec.val==0) {
+            
+        }
+    },1)  
+}
+
+//barre.addEventListener()
